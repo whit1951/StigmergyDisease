6f2296ea06d221fde8e073b1283df6e02efce50b
Lauren White <lwhite@rstudio03.research.sesync.org>
2019-06-02 10:44:17 -0400

    Submitting parallel reps for higher density treatments

--- Rslurm_parallel.R
+++ Rslurm_parallel.R
@@ -7,6 +7,8 @@
 source('~/StigmergyDisease/StigmergyLoopFunction.R')
 source('~/StigmergyDisease/MoveLoopFunction.R')
 source('~/StigmergyDisease/StigmergyFunctions.R')
+source('~/StigmergyDisease/StigmergyLoopRepFunction.R')
+
 
 # add functions from `Stigmergy Functions` file
 add_objects<-c("calc.dens", "Cmatrix", "create.prob", "createland", "get.neighbors", "index_val", "infect_direct", "infect_indirect", 
@@ -26,19 +28,28 @@ scent_decay<-c(0.1, 0.5)
 inf_decay<-c(0.1, 0.5)
 
 params<-expand.grid(maxT=maxT, nsim=nsim, lsize=lsize, n.initial=n.initial, inf_prob=inf_prob, rec_rate=rec_rate, dur_scent=dur_scent, initial_load=initial_load, scent_decay=scent_decay, inf_decay=inf_decay)
-# params<-params[1:8,] #subset of parameters
+params<-params[c(14, 16, 18, 32, 34, 36, 50,  52,  54,  68,  70,  72,  86,  88,  90, 104, 106, 108, 122, 124, 126, 140, 142, 144, 158, 160,
+                  162, 176, 178, 180, 194, 196, 198, 212, 214, 216, 230, 232, 234, 248, 250, 252, 266, 268, 270, 284, 286, 288),]
 
+nreps<-10
+params_reps<-data.frame()
+params_reps<-params[rep(seq_len(nrow(params)), each=nreps),]
+params_reps$nsim<-rep(10, times=nrow(params_reps))
+params_reps$nrep<-rep(1:10, times=nrow(params_reps)/nreps)
 
 library(rslurm)
 #options for slurm cluster
-sopt <- list(time = '120:00:00', share = TRUE, "mail-user"= "lwhite@sesync.org", "mail-type"="ALL", partition="sesync")
+sopt <- list(time = '144:00:00', share = TRUE, "mail-user"= "lwhite@sesync.org", "mail-type"="ALL", partition="sesync")
 
-sjob1 <- slurm_apply(StigLoop, params, jobname = 'stig_sim',
-                    nodes = 24, cpus_per_node = 8, submit = TRUE, add_objects=add_objects, slurm_options=sopt)
+sjob1 <- slurm_apply(StigLoop, params, jobname = 'stig_sim2',
+                    nodes = 6, cpus_per_node = 8, submit = TRUE, add_objects=add_objects, slurm_options=sopt)
 
-sjob2 <- slurm_apply(MoveLoop, params, jobname = 'move_sim',
-                     nodes = 24, cpus_per_node = 8, submit = TRUE, add_objects=add_objects, slurm_options=sopt)
+sopt2 <- list(time = '48:00:00', share = TRUE, "mail-user"= "lwhite@sesync.org", "mail-type"="ALL", partition="sesync")
+
+sjob2 <- slurm_apply(StigLoopRep, params_reps, jobname = 'stig_rep',
+                     nodes = 24, cpus_per_node = 8, submit = TRUE, add_objects=add_objects, slurm_options=sopt2)
 
 # Check on or cancel jobs:
 # print_job_status(sjob)
 # cancel_slurm(sjob)
+
