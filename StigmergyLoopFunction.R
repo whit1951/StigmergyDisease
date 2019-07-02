7934b77c5e10a548ef2afb4327a54091b7250ad2
Lauren White <lwhite@rstudio03.research.sesync.org>
2019-07-02 14:08:41 -0400

    Sub out scent_load and pathogen_load for dur_scent and initial_load; add dir_move arg; add dist_weights; get rid of direct transmission;

--- StigmergyLoopFunction.R
+++ StigmergyLoopFunction.R
@@ -11,25 +11,26 @@ source('~/StigmergyDisease/StigmergyFunctions.R')
 #'@param nsim - number of simulations to be repeated per parameter set
 #'@param lsize - landscape size (length of one dimension)
 #'@param n.initial - number of simulated agents on the landscape
-#'@param inf_prob - infection probability
 #'@param rec_rate - recovery rate
-#'@param dur_scent - how long scent marks last in the environment
-#'@param initial_load - initial pathogen load deposited into environment upon visiting a cell
+#'@param scent_load - how long scent marks last in the environment
+#'@param pathogen_load - initial pathogen load deposited into environment upon visiting a cell
 #'@param scent_decay - rate at which scent cues decay from the environment (N0*exp(-scent_decay*t))
 #'@param inf_decay - rate at which scent cues decay from the environment (N0*exp(-scent_decay*t))
-StigLoop<-function(maxT, nsim, lsize, n.initial, inf_prob, rec_rate, dur_scent, initial_load, scent_decay, inf_decay){
+#'@param dir_move - boolean (TRUE OR FALSE)- is movement directed in response to scent cues or random walk?
+StigLoop<-function(maxT, nsim, lsize, n.initial, rec_rate, scent_load, pathogen_load, scent_decay, inf_decay, dir_move){
   
   
   #Set up initial conditions/parameters to loop for simulations
   n.offset<-1 #neighborhood of nine cells, including current cell
   rowcol.delta <- expand.grid(-n.offset:n.offset,-n.offset:n.offset) #possible moves for given neighborhood size
-  prob_mat<-create.prob() #probability matrix governing movement choices after scent encounter
+  dist_weights<- get.dist.weight(rowcol.delta)
+  prob_mat<-create.prob(dist_weights) #probability matrix governing movement choices after scent encounter
   lxy<-longxy(lsize) #convenience data frame with x, y coordinates for number system of matrices in R
   
   ##Initiate data structure storage across simulations
   mydata<-list(inds=NULL, landscape=NULL, N=NULL, movedat=NULL, infdat=NULL)
-  filename<-paste("lsize", lsize, "n.initial", n.initial, "inf", inf_prob, "rec", rec_rate,"dur_scent",dur_scent,"initial_load",initial_load, "scent_decay", scent_decay, "inf_decay", inf_decay, sep="")
-  summary<-data.frame(lsize=rep(lsize, times=nsim), n.initial=rep(n.initial, times=nsim), inf_prob=rep(inf_prob, times=nsim), rec_rate=rep(rec_rate, times=nsim), dur_scent=rep(dur_scent, times=nsim), initial_load=rep(initial_load, times=nsim), scent_decay=rep(scent_decay, times=nsim), inf_decay= rep(inf_decay, times=nsim), duration=NaN, max_I=NaN, max_prevalence=NaN)
+  filename<-paste("lsize", lsize, "n.initial", n.initial, "rec", rec_rate,"scent_load",scent_load,"pathogen_load",pathogen_load, "scent_decay", scent_decay, "inf_decay", inf_decay, "dir_move", dir_move, sep="")
+  summary<-data.frame(lsize=rep(lsize, times=nsim), n.initial=rep(n.initial, times=nsim), rec_rate=rep(rec_rate, times=nsim), scent_load=rep(scent_load, times=nsim), pathogen_load=rep(pathogen_load, times=nsim), scent_decay=rep(scent_decay, times=nsim), inf_decay= rep(inf_decay, times=nsim), dir_move=rep(dir_move, times=nsim), duration=NaN, max_I=NaN, max_prevalence=NaN)
   infected<-matrix(NaN, nrow=maxT, ncol=nsim)
   
   
@@ -38,13 +39,10 @@ StigLoop<-function(maxT, nsim, lsize, n.initial, inf_prob, rec_rate, dur_scent, 
     inds<-make.inds(n.initial, lsize, nI=1) 
     
     # Create scent landscapes for each individual and for infection
-    landscape<-createland(lsize=lsize, inds=inds, dur_scent=dur_scent, infected= FALSE) #create array of landscapes to track scent mark location and strength for each animal
+    landscape<-createland(lsize=lsize, inds=inds, scent_load=scent_load, infected= FALSE) #create array of landscapes to track scent mark location and strength for each animal
     Num<-calc.dens(lsize=lsize, inds=inds)
-    inf_landscape<-Num[[3]]*initial_load
+    inf_landscape<-Num[[3]]*pathogen_load
     
-    #update which animals have been exposed to scent at t= 1
-    scented <-which(inds$vec %in% which(Num[[1]]>1))
-    inds$scent_exp[scented]<-1
     #record individual characteristics
     mydata$inds[[count]]<-inds
     
@@ -62,20 +60,23 @@ StigLoop<-function(maxT, nsim, lsize, n.initial, inf_prob, rec_rate, dur_scent, 
       possible_loc<-get.neighbors(inds[,2:3], mapdim=c(lsize,lsize), rowcol.delta=rowcol.delta, n.offset= n.offset, torus=TRUE, na.val=0)
       
       #sample among possible new cells and udate `inds` df
-      #newloc.vec<-apply(possible_loc, 1, sampfun) 
-      #inds<-new.cell(possible_loc,inds,lsize) #no aversion to scent marks
-      inds<-new.cell.directed(possible_loc,inds, prob_mat=prob_mat, lsize)
+      if(dir_move==TRUE){
+        inds<-new.cell.directed(possible_loc,inds, prob_mat=prob_mat, lsize, scent_load=scent_load)
+      }
+      if(dir_move==FALSE){
+        inds<-new.cell(possible_loc,inds,lsize, dist_weights=dist_weights) #no aversion to scent marks
+      }
       movedat<-cbind(movedat, inds$vec)
       
       
       #Disease processes
       #Which individuals become directly infected by other conspecfics?
-      Num<-calc.dens(lsize=lsize, inds=inds)
-      inds<-infect_direct(inds, nS=as.numeric(Num[[2]]), nI=as.numeric(Num[[3]]), transProb=inf_prob, lxy=lxy)
-      
+      # Num<-calc.dens(lsize=lsize, inds=inds)
+      # inds<-infect_direct(inds, nS=as.numeric(Num[[2]]), nI=as.numeric(Num[[3]]), transProb=inf_prob, lxy=lxy)
+      #
       #Which individuals becom indirectly infected by environmental exposure?
       Num<-calc.dens(lsize=lsize, inds=inds) #recalculate S vs. I after direct transmission
-      inds<-infect_indirect(inds, nS=as.numeric(Num[[2]]), nI=as.numeric(Num[[3]]), transProb=inf_prob, lxy=lxy, inf_landscape=inf_landscape)
+      inds<-infect_indirect(inds, nS=as.numeric(Num[[2]]), nI=as.numeric(Num[[3]]), lxy=lxy, inf_landscape=inf_landscape)
       
       #Which animals recover?
       inds<-recover.inds(inds, gamma=rec_rate)
@@ -85,27 +86,24 @@ StigLoop<-function(maxT, nsim, lsize, n.initial, inf_prob, rec_rate, dur_scent, 
       N[t,] <- c(sum(inds$status=="S"), sum(inds$status=="I"), sum(inds$status=="R"))
       
       #Update scent mark landscapes (if time remaining on landscape is greater than one, subtract one time step)
-      #landscape[which(landscape>1)]<-landscape[which(landscape>1)]-1
-      landscape[which(landscape>1)]<-landscape[which(landscape>1)]*exp(-scent_decay*1)
+      landscape<-landscape*exp(-scent_decay*1)
       
       for(i in 1:n.initial)
       {
         #array[row, col, layer]
-        landscape[inds$y[i], inds$x[i], i]<-dur_scent #at new location, deposit scent mark of initial strength
+        landscape[inds$y[i], inds$x[i], i]<-scent_load #at new location, deposit scent mark of initial strength
       }
       
       #Exposed to scent mark in this latest move? if sum of scent layers > individual deposition, exposure = Y
-      #scent_sum<-apply(landscape, MARGIN=c(1, 2), sum)
       for(j in 1:n.initial)
       {
         scent_sum<-apply(landscape[,,-j], MARGIN=c(1, 2), sum) #sum scent landscapes minus the current individual's profile
-        inds$scent_exp[j]<-ifelse(scent_sum[inds$vec[j]]>0, 1, 0) #if scent lingers in the new cell, assign a value of 1, otherwise 0
+        inds$scent_exp[j]<-scent_sum[inds$vec[j]] #reassign scent exposure value (can be greater than one)
       }
       
       # Update infection landscape (assume that deposit of infectious agents is additive/cumulative)
-      #inf_landscape[which(inf_landscape>1)]<-inf_landscape[which(inf_landscape>1)]-1
-      inf_landscape[which(inf_landscape>1)]<-inf_landscape[which(inf_landscape>1)]*exp(-inf_decay*1)
-      inf_landscape<-Num[[3]]*initial_load+inf_landscape
+      inf_landscape<-inf_landscape*exp(-inf_decay*1)
+      inf_landscape<-Num[[3]]*pathogen_load+inf_landscape
       
       if(sum(inds$status=="I")==0){ #If number of infected in individuals--> 0, stop running
         summary$duration[count]<-t-1
@@ -133,6 +131,6 @@ StigLoop<-function(maxT, nsim, lsize, n.initial, inf_prob, rec_rate, dur_scent, 
 
 #Test
 
-# tic=Sys.time()
-# StigLoop(maxT=1000, nsim=10, lsize=50, n.initial=100, inf_prob=0.1, rec_rate=0.1, dur_scent=10, initial_load=10, scent_decay=0.1, inf_decay=0.1)
-# print(difftime(Sys.time(),tic,units="mins"))
+tic=Sys.time()
+StigLoop(maxT=200, nsim=2, lsize=50, n.initial=50, rec_rate=0.01, scent_load=0.1, pathogen_load=10, scent_decay=0.01, inf_decay=0.01, dir_move=FALSE)
+print(difftime(Sys.time(),tic,units="mins"))
