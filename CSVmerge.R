12752449c40694af4aff9ba93d6759102936fa36
Lauren White <lwhite@rstudio03.research.sesync.org>
2019-09-12 16:29:55 -0400

    Update facet grid labels

--- CSVmerge.R
+++ CSVmerge.R
@@ -64,6 +64,8 @@ merged_data$SL[which(merged_data$scent_load==10)]<-"highSL"
 merged_data$decay <- paste(merged_data$scent_decay,merged_data$inf_decay)
 setwd("~/StigmergyDisease/")
 # write.csv(merged_data, "stig_summary.csv")
+
+
 merged_data<-read.csv("stig_summary.csv")
 #Order factors
 # merged_data$betas<- factor(merged_data$betas, levels = c("0 0 0", "0 0 -1", "0 1 -1", "0 1 -0.5", "0 2 -1", "0 2 -0.5", "3 0 0", "3 0 -1", "3 1 -1", "3 1 -0.5", "3 2 -1", "3 2 -0.5", "6 0 0", "6 0 -1", "6 1 -1", "6 1 -0.5", "6 2 -1", "6 2 -0.5"))
@@ -199,7 +201,7 @@ gg
 
 
 ##Heatmap duration for n.initial=50 (Initial deposits)
-sdf <- summaryBy(duration~rec_rate+scent_load +pathogen_load +scent_decay + inf_decay +dir_move, data=sub_n50_dT_r0.01, FUN=mean)
+sdf <- summaryBy(duration~rec_rate+scent_load +pathogen_load +scent_decay + inf_decay +dir_move, data=size50, FUN=mean)
 gg <- ggplot(sdf, aes(x=as.factor(dir_move), y=as.factor(rec_rate), fill=duration.mean))
 gg <- gg + geom_tile(color="white", size=0.1)
 gg <- gg + scale_fill_viridis(name="Duration")
@@ -277,7 +279,9 @@ sub_data<-merged_data[which(merged_data$max_I>1),] #successful outbreaks
 #tiff("boxplot_prev.tiff", height = 8, width = 12, units = "in", compression = "lzw", res = 300)
 gg<- ggplot(data=size100, aes(y=max_prevalence, x=as.factor(rec_rate)))
 #gg<- gg+ geom_bar(stat="identity", position=position_dodge(), colour="black")+ facet_grid(Rec.prob. ~ percep, scales= "free_x", labeller=label_both)
-gg<-gg+ geom_boxplot()+ facet_grid( dir_move~ inf_decay, scales= "free_x", labeller=label_value)
+gg<-gg+ geom_boxplot()+ facet_grid(  inf_decay~dir_move, scales= "free_x", labeller= labeller(
+  dir_move = c("FALSE" = "Random", "TRUE"="Stigmergy"),
+  inf_decay = c("0.01" = "Slow pathogen decay", "0.1"="Med. pathogen decay", "1" = "Fast pathogen decay")))
 gg<- gg+ labs(x = expression(Recovery~rate~(gamma)), y = "Maximum prevalence") #,title="(A)")
 #gg<- gg+ theme(axis.title.x = element_text(face="bold", size=10),title=element_text(face="bold", size=10))
 gg<- gg + theme(axis.text = element_text(size=20))
@@ -288,6 +292,24 @@ gg<- gg + theme(plot.title = element_text(size = 18))
 gg<- gg + theme(plot.margin=unit(c(0.1,0.1,0,0.1), "cm"))
 #gg<-gg + geom_text(data = means , aes(label = duration, y = duration + 0.08))
 gg
+
+gg<- ggplot(data=size100, aes(y=duration, x=as.factor(rec_rate)))
+#gg<- gg+ geom_bar(stat="identity", position=position_dodge(), colour="black")+ facet_grid(Rec.prob. ~ percep, scales= "free_x", labeller=label_both)
+gg<-gg+ geom_boxplot()+ facet_grid(  inf_decay~dir_move, scales= "free_x", labeller= labeller(
+  dir_move = c("FALSE" = "Random", "TRUE"="Stigmergy"),
+  inf_decay = c("0.01" = "Slow pathogen decay", "0.1"="Med. pathogen decay", "1" = "Fast pathogen decay")))
+gg<- gg+ labs(x = expression(Recovery~rate~(gamma)), y = "Duration") #,title="(A)")
+#gg<- gg+ theme(axis.title.x = element_text(face="bold", size=10),title=element_text(face="bold", size=10))
+gg<- gg + theme(axis.text = element_text(size=20))
+gg<- gg + theme(axis.text.x =element_text(vjust= 0, angle=-90))
+gg<- gg + theme(axis.title = element_text(vjust=0.5, size=18))
+gg<- gg + theme(strip.text = element_text(vjust=0.5, size=18))
+gg<- gg + theme(plot.title = element_text(size = 18))
+gg<- gg + theme(plot.margin=unit(c(0.1,0.1,0,0.1), "cm"))
+#gg<-gg + geom_text(data = means , aes(label = duration, y = duration + 0.08))
+gg
+
+
 
 ii<- ggplot(data=sub_n100_dT_r0.01, aes(x=as.factor(scent_load), y=max_prevalence, fill=as.factor(pathogen_load)))
 ii<- ii+ geom_boxplot() #+ geom_jitter(alpha=0.5)
