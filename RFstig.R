4edb8b67ea984e30cdcd800d9e2e236ca4abc6c8
Lauren White <lwhite@rstudio03.research.sesync.org>
2020-01-08 12:12:35 -0500

    Add train/test data sets

--- RFstig.R
+++ RFstig.R
@@ -2,75 +2,135 @@
 #' @date May 29, 2019
 #' @author Lauren White
 
-#Load libraries
+#Load libraries and set seed
 library(parallel)
 library(doParallel)
 library(foreach)
 library(randomForest)
+library(caret)
+
+set.seed(123)
+setwd("~/StigmergyDisease")
 
 #Testing
 #Detect cores
 ncores <- parallel::detectCores()
 doParallel::registerDoParallel(ncores)
 
-
 #Load data
 merged_data<-read.csv("~/StigmergyDisease/stig_summary.csv")
 merged_data<-merged_data[,-1]
-# merged_data<-subset(merged_data, rec_rate==0.01)
-# merged_data<-merged_data[which(merged_data$n.initial==125),]
-merged_data$outbreak<-ifelse(merged_data$max_I>1, 1,0)
+
+merged_data$outbreak<-ifelse(merged_data$max_I>1, 1, 0) #create an outbreak success column, if successful=1, if unsuccessful=0
+merged_data$outbreak<-as.factor(merged_data$outbreak)
+
+samp <- sample(nrow(merged_data), 0.8 * nrow(merged_data))
+train <- merged_data[samp, ]
+test <- merged_data[-samp, ]
 
 
-x<-merged_data[,c(2:7)] #covariates
-y<-merged_data$outbreak #response variable, outbreak (0 or 1)
+# Outbreak success --------------------------------------------------------
 
+x_train<-train[,c(2:8)] #covariates
+y_train<-train$outbreak #response variable, categorical, outbreak (0 or 1)
 
 tic=Sys.time()
-set.seed(123)
-rf_logit <- foreach(ntree=rep(100, 100), .combine=combine, .multicombine=TRUE,
-              .packages='randomForest') %dopar% {
-                randomForest(x, y, ntree=ntree, importance=TRUE)
-              }
+# (rf_logit <- foreach(ntree=rep(10, 10), .combine=combine, .multicombine=TRUE,
+#               .packages='randomForest') %dopar% {
+#                 randomForest(x_train, y_train, ntree=ntree, importance=TRUE)
+#               })
+(rf_logit<-randomForest(x_train, y_train, ntree=1000, importance=TRUE))
 print(difftime(Sys.time(),tic,units="mins"))
+plot(rf_logit)
+
+OOB_mat<-table(predict(rf_logit),train$outbreak)
+
+(OOB_error<-(OOB_mat[1,2]+OOB_mat[2,1])/(OOB_mat[1,2]+OOB_mat[2,1]+ OOB_mat[1,1]+OOB_mat[2,2])) #OOB error estimate
+
+pred <- predict(rf_logit, newdata = test)
+conf_mat<-table(pred, test$outbreak)
+(acc<-(conf_mat[1,1]+conf_mat[2,2])/(conf_mat[1,2]+conf_mat[2,1]+ conf_mat[1,1]+conf_mat[2,2]))
 # save(rf_logit, file = "RF_logit.RData")
 
-#varImpPlot(rf, type=2, main= "Duration")
+# rf_fit <- train(x_train, y_train, method = "rf")
 
-setwd("~/StigmergyDisease")
+varImpPlot(rf_logit, type=1, main= "Outbreak Success")
+
+
 imp_logit<-rf_logit$importance
 write.csv(imp_logit, "imp_logit.csv")
 
 
+# Maximum prevalence ------------------------------------------------------
+
 merged_data<-subset(merged_data, merged_data$outbreak==1) #analyze only those outbreaks that spread beyond initial individual
-x<-merged_data[,c(2:7)] #covariates
+merged_data$duration<-merged_data$duration/mean(merged_data$duration) #normalize duration, since not contained between 0 and 1
+
+samp <- sample(nrow(merged_data), 0.8 * nrow(merged_data))
+train <- merged_data[samp, ]
+test <- merged_data[-samp, ]
+
+x_train<-train[,c(2:8)] #covariates
+y_train<-train$max_prevalence
 
 tic=Sys.time()
-set.seed(123)
-# y<-merged_data$max_I/mean(merged_data$maxI) #response variable, maxI
-y<-merged_data$max_prevalence
-rf_prev <- foreach(ntree=rep(100, 100), .combine=combine, .multicombine=TRUE,
-                    .packages='randomForest') %dopar% {
-                      randomForest(x, y, ntree=ntree, importance=TRUE)
-                    }
+# (rf_prev <- foreach(ntree=rep(10, 10), .combine=combine, .multicombine=TRUE,
+#                     .packages='randomForest') %dopar% {
+#                       randomForest(x_train, y_train, ntree=ntree, importance=TRUE)
+#                     })
+(rf_prev<-randomForest(x_train, y_train, ntree=1000, importance=TRUE))
 print(difftime(Sys.time(),tic,units="mins"))
+plot(rf_prev)
+
+predicted<-unname(predict(rf_prev, test))
+actual <- test$max_prevalence
+
+R2 <- 1 - (sum((actual-predicted)^2, na.rm=TRUE)/sum((actual-mean(actual))^2, na.rm=TRUE))
 # save(rf_prev, file = "RF_prev.RData")
+
+varImpPlot(rf_prev, type=1, main= "Prevalence")
 
 imp_prev<-rf_prev$importance
 write.csv(imp_prev, "imp_prev.csv")
 
 
+# Duration ----------------------------------------------------------------
+
+y_train<-train$duration
+
 tic=Sys.time()
-set.seed(123)
-y<-merged_data$duration/mean(merged_data$duration) #response variable, duration
-rf_dur <- foreach(ntree=rep(100, 100), .combine=combine, .multicombine=TRUE,
-                   .packages='randomForest') %dopar% {
-                     randomForest(x, y, ntree=ntree, importance=TRUE)
-                   }
+# rf_dur <- foreach(ntree=rep(10, 10), .combine=combine, .multicombine=TRUE,
+#                    .packages='randomForest') %dopar% {
+#                      randomForest(x, y, ntree=ntree, importance=TRUE)
+#                    }
+(rf_dur<-randomForest(x_train, y_train, ntree=1000, importance=TRUE))
 print(difftime(Sys.time(),tic,units="mins"))
 # save(rf_dur, file = "RF_dur.RData")
 
+plot(rf_dur)
+predicted<-unname(predict(rf_dur, test))
+actual <- test$duration
+
+(R2 <- 1 - (sum((actual-predicted)^2, na.rm=TRUE)/sum((actual-mean(actual))^2, na.rm=TRUE)))
+# save(rf_prev, file = "RF_prev.RData")
+
+varImpPlot(rf_dur, type=1, main= "Duration")
 imp_dur<-rf_dur$importance
 write.csv(imp_dur, "imp_dur.csv")
 
 
+# Test calculation of R2 for combined randomforest ------------------------
+# 
+# tic=Sys.time()
+# (rf_prev<-randomForest(x_train, y_train, ntree=1000, importance=TRUE))
+# print(difftime(Sys.time(),tic,units="mins"))
+# 
+# rf_prev$mse
+# rf_prev$rsq*100 #percent variance explained
+# 
+# (predicted<-unname(predict(rf_prev, train)))
+# predicted<-unname(rf_prev$predicted)
+# actual <- train$max_prevalence
+# # predicted <- unname(predict(rfPar, dat))
+# 
+# R2 <- 1 - (sum((actual-predicted)^2, na.rm=TRUE)/sum((actual-mean(actual))^2, na.rm=TRUE))
